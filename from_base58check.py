5b528c5ffa07e50b93d8f8151748056c52739d60
takisal <33737751+takisal@users.noreply.github.com>
2023-12-14 17:39:13 -0500

    Update from_base58check.py

--- from_base58check.py
+++ from_base58check.py
@@ -2,6 +2,13 @@ import bitcoin
 import sys
 
 x = sys.argv[1]
-print(bitcoin.encode_privkey(x,'wif')+"\n")
 
-print(bitcoin.encode_privkey(x,'wif_compressed')+"\n")
+def hex_to_base58check(x, compressed):
+    if compressed:
+        return bitcoin.encode_privkey(x,'wif_compressed')+"\n"
+    else:
+        return bitcoin.encode_privkey(x,'wif')+"\n"
+
+
+def main():
+    print(hex_to_base58check(sys.argv[1]))
